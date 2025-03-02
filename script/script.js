0ae3bb5fe8ba66e43d9418d77dca0a9d92847d37
moriammunni <munnimoriam51@gmail.com>
2025-03-02 08:19:39 +0600

    Design complete

---
+++ script/script.js
@@ -0,0 +1,14 @@
+
+    function changeBackgroundColor() {
+        const colors = ["#F0F8FF", "#FAEBD7", "#E6E6FA", "#FFD700", "#98FB98", "#FFB6C1"];
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+        document.body.style.backgroundColor = randomColor;
+    }
+
+    document.body.addEventListener("click", changeBackgroundColor);
+
+document.getElementById('new-today')
+  .addEventListener('click',function(event){
+    event.preventDefault();
+    window.location.href='./main.html'
+  })
